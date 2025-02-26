97e6bf3e11990b5c7252106721e28f8d0e6f32f3
djtork3 <asis1989@hotmail.es>
2025-02-26 23:03:29 +0100

    DexTorrentGo2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // index.js
 import pkg from 'stremio-addon-sdk';
 const { addonBuilder, serveHTTP } = pkg;
@@ -5,18 +6,28 @@ import { handleMetadata } from './metadataHandler.js';
 import { handleStream } from './streamHandler.js';
 import { handleCatalog } from './catalogHandler.js';
 
-const port = process.env.PORT || 8000; // Usar el puerto asignado por Koyeb
-
 const builder = new addonBuilder({
     "id": "My-DexTorrentGo",
     "name": "DexTorrentGo",
     "version": "1.0.2",
-    "resources": ["catalog", "stream", "meta"],
-    "types": ["movie", "series", "telenovelas"],
+    "resources": ["catalog", "stream", "meta"],  // Incluye los tres recursos
+    "types": ["movie", "series", "telenovelas"],  // Tipos de contenido soportados
     "catalogs": [
-      { "type": "movie", "id": "my-movie-catalog", "name": "DexTorrentGo" },
-      { "type": "series", "id": "my-series-catalog", "name": "DexTorrentGo" },
-      { "type": "telenovelas", "id": "my-telenovela-catalog", "name": "DexTorrentGo" }
+      {
+        "type": "movie",  // Catálogo para películas
+        "id": "my-movie-catalog",
+        "name": "DexTorrentGo"
+      },
+      {
+        "type": "series",  // Catálogo para series
+        "id": "my-series-catalog",
+        "name": "DexTorrentGo"
+      },
+      {
+        "type": "telenovelas",  // Catálogo para telenovelas
+        "id": "my-telenovela-catalog",
+        "name": "DexTorrentGo"
+      }
     ],
     "background": "https://i.ibb.co/LDDm7Mtn/ab1d7366-fae1-4d35-9ebb-8823a1de85f5.png",
     "logo": "https://i.ibb.co/jvZWxGLS/logo.png",
@@ -30,4 +41,6 @@ builder.defineCatalogHandler(handleCatalog);
 builder.defineStreamHandler(handleStream);
 builder.defineMetaHandler(handleMetadata);
 
+const port = process.env.PORT || 7000; // Usar el puerto asignado por Koyeb
 serveHTTP(builder.getInterface(), { port });
+
