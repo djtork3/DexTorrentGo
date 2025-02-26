85f4099a73fb1242353489c905820fad1dcc9dd6
djtork3 <asis1989@hotmail.es>
2025-02-27 00:34:40 +0100

    DexTorrentGo2

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-node index.js &
-node server.js &
-wait
+set -e
+
+# Iniciar los procesos en segundo plano con salida a logs
+node index.js >> index.log 2>&1 &
+INDEX_PID=$!
+
+node server.js >> server.log 2>&1 &
+SERVER_PID=$!
+
+# Esperar a que terminen
+wait $INDEX_PID
+wait $SERVER_PID
